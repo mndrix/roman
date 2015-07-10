3ee110049a56eaf3c774117facc54b42be0d618e
Michael Hendricks <michael@ndrix.org>
2015-07-10 15:56:46 -0600

    Implementation with tests

--- prolog/roman.pl
+++ prolog/roman.pl
@@ -1,3 +1,32 @@
-:- module(roman, []).
+:- module(roman, [dcg//1, roman/2]).
 
-% TODO implement the module
+:- use_module(library(clpfd)).
+
+roman(Arabic,Roman) :-
+    phrase(dcg(Arabic),Roman).
+
+dcg(Arabic) -->
+    { Arabic in 1..3999 },
+    roman(Arabic),
+    !.
+
+roman(Total) -->
+    { Rest #>= 0 },
+    { Total #= Value + Rest },
+    digit(Value),
+    roman(Rest).
+roman(0) --> "".
+
+digit(1000) --> "M".
+digit(900) --> "CM".
+digit(500) --> "D".
+digit(400) --> "CD".
+digit(100) --> "C".
+digit(90) --> "XC".
+digit(50) --> "L".
+digit(40) --> "XL".
+digit(10) --> "X".
+digit(9) --> "IX".
+digit(5) --> "V".
+digit(4) --> "IV".
+digit(1) --> "I".
